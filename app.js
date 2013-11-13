1cb65c9ce62dbdf3373d5c3a786dd15a0863b6f0
xidui <apple@MacBook-Pro-2.local>
2013-11-13 17:14:42 +0800

    路由控制的部分代码

--- app.js
+++ app.js
@@ -5,17 +5,20 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var ejs = require('ejs');
+//var html = require('html');
 
 var app = express();
 
 // all environments
+app.engine('.html', ejs.__express);
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.bodyParser());// add myself
+app.set('view engine', 'html');
+//app.use(express.bodyParser({uploadDir:'./uploads'}));
+app.use(express.bodyParser());
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -29,8 +32,8 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+routes.gets(app);
+routes.posts(app);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,36 @@
 /*
  * GET home page.
  */
-
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
+ var fs = require('fs');
+ var address='/Users/apple/Documents/github/lazy/upload/';
+ 
+ exports.gets = function(app) {
+ 	app.get('/', function (req, res) {
+    	res.render('index', { title: 'Express' });
+  	});
+  	app.get('/success',function (req,res) {
+  		console.log(req.param('abc'));
+  		console.log(req.param('efg'));
+  		console.log(req.query.abc);
+   		res.render('success');
+   	});
+ };
+ 
+ exports.posts =function(app){
+ 	app.post('/file-upload', function(req, res) {
+ 	     // 获得文件的临时路径
+ 	     var tmp_path = req.files.thumbnail.path;
+ 	     console.log(tmp_path);
+ 	     // 指定文件上传后的目录 - 示例为"images"目录。 
+ 	     var target_path = address + req.files.thumbnail.name;
+ 	     // 移动文件
+ 	     fs.rename(tmp_path, target_path, function(err) {
+ 	     	if (err) throw err;
+ 	      	// 删除临时文件夹文件, 
+ 	      	fs.unlink(tmp_path, function() {
+ 	         	if (err) throw err;
+ 	         	res.send('File uploaded to: ' + target_path + ' - ' + req.files.thumbnail.size + ' bytes');
+ 	      	});
+ 	    });
+ 	  });
+ }
